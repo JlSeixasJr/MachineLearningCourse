f61ed35019268ec26b125e8a791170df038ede4a
José Luis Seixas Junior <jlseixasjr@gmail.com>
2022-04-22 16:58:53 +0200

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,31 @@
+####################### SSH KEY FOR GIT ###################################
+eval `ssh-agent -s` && ssh-add -k
+####################### To be added to git account settings ################
+
+###################### GIT PARAMETERS #####################################
+export GIT_PARENT_DIR=~
+export GIT_REPO_NAME=#your-repo
+export GIT_BRANCH_NAME=#your-branch
+export GIT_USER=#your-gituser
+export GIT_EMAIL=#your-email
+export GITHUB_ACCESS_TOKEN=#access-token from github developer settings
+export GIT_USER_UPSTREAM=#your-user
+
+############################################################################
+#### DO NOT EDIT BELOW THIS LINE: derived variables
+############################################################################
+export GIT_REMOTE_URL=git@github.com:$GIT_USER/$GIT_REPO_NAME.git
+export GIT_REMOTE_URL_HTTPS=https://github.com/$GIT_USER/$GIT_REPO_NAME.git
+export GIT_REMOTE_UPSTREAM=$GIT_USER_UPSTREAM/$GIT_REPO_NAME
+
+####################### Git Repo where notebooks will be pushed ############
+cd $GIT_PARENT_DIR && git clone $GIT_REMOTE_URL_HTTPS
+
+####################### Change in jupyter config ###########################
+if [ ! -f ~/.jupyter/jupyter_notebook_config.py ]; then
+   jupyter notebook --generate-config
+fi
+
+echo 'c.NotebookApp.disable_check_xsrf = True' >> ~/.jupyter/jupyter_notebook_config.py
+
+cp $GIT_PARENT_DIR/githubcommit/config ~/.ssh/config
